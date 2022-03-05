9d25ed3aa9f89704068eb4b780e91ac469c4a22f
Ksenia <naomi355@yandex.ru>
2022-03-05 12:37:48 +0300

    Добавление заглушки на случай, если нет простых чисел

--- index.js
+++ index.js
@@ -14,21 +14,24 @@ function getPrimes(ending) {
         }
     }
 
-    if (primes.length === 0) {
-        return console.log("Empty");
-    } else return primes;
+
+    return primes;
 }
 
 function coloredNumbers(primes) {
-    for (let i = 0; i < primes.length; i += 3) {
-        if (primes[i]) {
-            console.log(colors.green(primes[i]));
-        }
-        if (primes[i + 1]) {
-            console.log(colors.yellow(primes[i + 1]));
-        }
-        if (primes[i + 2]) {
-            console.log(colors.red(primes[i + 2]));
+    if (primes.length === 0) {
+        return console.log("В заданном диапазоне нет простых чисел.");
+    } else {
+        for (let i = 0; i < primes.length; i += 3) {
+            if (primes[i]) {
+                console.log(colors.green(primes[i]));
+            }
+            if (primes[i + 1]) {
+                console.log(colors.yellow(primes[i + 1]));
+            }
+            if (primes[i + 2]) {
+                console.log(colors.red(primes[i + 2]));
+            }
         }
     }
 }
@@ -37,3 +40,32 @@ let end = parseInt(process.argv[2]);
 if (!Number.isNaN(end)) {
     coloredNumbers(getPrimes(end));
 } else { console.log("Ошибка. Необходимо ввести число."); }
+
+
+
+
+
+// let timer = [];
+// const EventEmitter = require('events');
+// for (let i = 2; i < process.argv.length; i++) {
+//     timer.push(process.argv[i]);
+// }
+
+
+// for (let i = 0; i < timer.length; i++) {
+//     let arg = timer[i].split('-',)
+//     let hour = parseInt(arg[0]);
+//     let day = parseInt(arg[1]);
+//     let month = parseInt(arg[1]);
+//     let year = parseInt(arg[1]);
+
+// }
+
+
+
+// console.log(timer);
+
+// console.log(hour);
+// console.log(day);
+// console.log(month);
+// console.log(year);
